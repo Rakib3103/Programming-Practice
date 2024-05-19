81c53af0eea37a97d8f19eb71f5f52776929db09
Mazharul Islam Rakib <mazharul.islam.rakib@g.bracu.ac.bd>
2024-05-19 13:34:49 +0600

    C++

---
+++ C++ Practice/NumericTypes.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int myNum1 = 1000;
+    double myNum2 = 19.22;
+    float myNum3 = 5e78;
+
+    cout << myNum1 << endl;
+    cout << myNum2 << endl;
+    cout << myNum3 << endl;
+    
+    return 0;
+}
---
+++ C++ Practice/Variables.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int myNum = 15;
+    double myFloatNum = 5.99;
+    char myLetter = 'D';
+    string myText = "Hello!";
+    bool myBoolean = true;
+
+
+    std::cout << myNum << endl;
+    std::cout << myFloatNum << endl;
+    std::cout << myLetter << endl;
+    std::cout << myText << endl;
+    std::cout << myBoolean << endl;
+    return 0;
+}
