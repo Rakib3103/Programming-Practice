dbdfc5d886b95dc77b8c9b4b6d23f1f6896a9fb6
Mazharul Islam Rakib <mazharulislam31032001@gmail.com>
2024-05-25 00:15:23 +0600

    Latest

---
+++ JAVA Practice/ForEachLoop.java
@@ -0,0 +1,9 @@
+public class ForEachLoop {
+    public static void main(String[] args) {
+        String[] cars = {"Volvo", "BMW", "Ford", "Corolla"};
+        for (String i : cars){
+            System.out.println("i");
+        }
+    }
+    
+}
